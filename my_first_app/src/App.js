0391a31f5c1bd3df924db1f29bba39c3170db662
mahiboobnadaf <rasool97260@gmail.com>
2024-10-10 02:37:07 +0530

    created a counter app

--- my_first_app/src/App.js
+++ my_first_app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [count,setCount] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
 
+  let id;
+  const start = () =>{
+    setIsClicked(true);
+  }   
+  
+  const stop = () =>{
+    setCount(count)
+    setIsClicked(false)
+  }
+
+  const reset = () =>{
+    setCount(0)
+    setIsClicked(false)
+  }
+
+  useEffect(()=>{ 
+    id = setInterval(()=>{
+      if(isClicked){
+        setCount(count=>count+1)
+    }},1000);
+  
+    return ()=>clearInterval(id);
+  },[start]);
+
+  return (
+    <>
+      <div>
+        <h1 style={{display:"flex",color:"green"}}> My-Counter</h1>
+        <h1 > {count} </h1>
+        <button onClick={start} > Start/Resume </button>
+        <button onClick={stop} > Stop </button>
+        <button onClick={reset}>  Reset </button>
+
+      </div>
+    </>
+   
+  );
+
+}
 export default App;
