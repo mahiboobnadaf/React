7403d157657e6edafc35ee358cbc71e406f15f5c
mahiboobnadaf <rasool97260@gmail.com>
2024-10-20 18:53:20 +0530

    image added

--- learning_react-2/src/ImageSlider.js
+++ learning_react-2/src/ImageSlider.js
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react";
 const data = ["https://wallpapers.com/images/hd/random-background-1280-x-800-9lscltpwc61kjbj2.jpg",
     "https://img.freepik.com/free-vector/gradient-black-background-with-cubes_23-2149160350.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtNU2MmTmOmYk33WelK7LssKRyPtssMjpE0w&s",
-    "https://wallpapers.com/images/hd/random-pictures-zxti3l8kfhl5ojbh.jpg"
+    "https://wallpapers.com/images/hd/random-pictures-zxti3l8kfhl5ojbh.jpg",
+    "https://img.freepik.com/free-photo/beautiful-mountains-landscape_23-2151151081.jpg"
 ]
 
 const ImageSlider = () =>{
